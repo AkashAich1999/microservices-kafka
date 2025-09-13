873f28f3e92b6a1969d22939faebe6d8a7eb1253
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2025-09-13 22:29:05 +0530

    Docker Kafka Cluster with KRaft

---
+++ 3-microservices-kafka-cluster/services/payment-service/index.js
@@ -0,0 +1,53 @@
+import express from "express"
+import cors from "cors"
+import { Kafka } from "kafkajs"
+
+const app = express();
+
+app.use(cors({
+    origin:"http://localhost:3000"
+}));
+
+app.use(express.json());
+
+const kafka = new Kafka({
+    clientId: "payment-service",
+    brokers: ["localhost:9094", "localhost:9095", "localhost:9096"],
+});
+
+const producer = kafka.producer();
+
+const connectToKafka = async () => {
+    try {
+        await producer.connect();
+        console.log("Producer Connected !");
+    } catch (err) {
+        console.log("Error Connecting to Kafka", err);
+    }
+}
+
+app.post("/payment-service", async (req, res) => {
+    const { cart } = req.body
+    // Assume that we get the Cookie & Decrypt the User ID.
+    const userId = "123"
+
+    // TODO: PAYMENT
+    console.log("API Endpoint Hit!");
+
+    // KAFKA
+    await producer.send({
+        topic: "payment-successful",
+        messages: [{ value: JSON.stringify({ userId, cart }) }]
+    })
+
+    return res.status(200).send("Payment Successful");
+})
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).send(err.message)
+});
+
+app.listen(8000, () => {
+    connectToKafka();
+    console.log("Payment Service is Running on PORT 8000");
+});
